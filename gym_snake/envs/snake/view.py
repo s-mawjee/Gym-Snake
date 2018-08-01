51414b02cfce712626b466020337cb101e5cdf78
Andreas Pasternak <andreas.pasternak@gmx.ch>
2018-08-01 19:38:29 +0200

    -------------------------------------- Saving model due to mean reward increase: 31.8 -> 32.0 Saving model due to mean reward increase: 32.0 -> 32.1 Saving model due to mean reward increase: 32.1 -> 32.4 Saving model due to mean reward increase: 32.4 -> 32.6 Saving model due to mean reward increase: 32.6 -> 32.9 -------------------------------------- | % time spent exploring  | 1        | | episodes                | 5100     | | mean 100 episode reward | 32.6     | | steps                   | 266028   | -------------------------------------- Saving model due to mean reward increase: 32.9 -> 33.1

--- gym_snake/envs/snake/view.py
+++ gym_snake/envs/snake/view.py
@@ -18,7 +18,7 @@ class LocalView:
         self.grid = grid
         self.prev_action = Snake.DOWN
 
-    def get(self, offset = (0,0), action = None ):
+    def get(self, offset = (0,0), action = None):
         assert(np.array_equal(np.asarray(self.grid.grid.shape[0:2]) % 2, np.asarray([1, 1])))
         local_grid = np.zeros((self.grid.grid.shape[0]*2+1, self.grid.grid.shape[1]*2+1), np.int8)
         grid_size = np.array(self.grid.grid.shape[0:2], dtype=np.int8)
@@ -30,11 +30,14 @@ class LocalView:
 
         local_grid[start[0]:end[0],  start[1]:end[1]] = self.grid.grid
 
-        # print("ACITON "+str(action))
         if(action):
             local_grid = np.rot90(local_grid, -self.get_rotation(action))
 
         self.prev_action = action
+
+        # Set own head to free space color. We do not want to have our own head to have as strong effect:
+        local_grid[local_grid.shape[0] // 2, local_grid.shape[1] // 2] = self.grid.SPACE_COLOR
+
         # plt.imshow(local_grid, interpolation='none')
         # plt.show()
 
--- train.py
+++ train.py
@@ -3,26 +3,26 @@ from baselines.common import set_global_seeds
 from baselines import bench
 import argparse
 from baselines import logger
-
+import os
 import gym_snake.envs.snake_env
 from gym_snake.envs.snake.view import LocalAction
 from gym_snake.envs.snake.snake import Snake
 
+
 def main():
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('--env', help='environment ID', default='BreakoutNoFrameskip-v4')
-    parser.add_argument('--seed', help='RNG seed', type=int, default=0)
-    parser.add_argument('--prioritized', type=int, default=1)
-    parser.add_argument('--prioritized-replay-alpha', type=float, default=0.6)
-    parser.add_argument('--dueling', type=int, default=1)
-    parser.add_argument('--num-timesteps', type=int, default=int(100000))
-    parser.add_argument('--checkpoint-freq', type=int, default=100)
-    parser.add_argument('--checkpoint-path', type=str, default='/home/pasa/deeplearning/tf_models/snake')
-
+    parser.add_argument('--gpu', help='GPU to use', default='0')
     args = parser.parse_args()
+
+    os.environ["CUDA_VISIBLE_DEVICES"] = args.gpu
+
+    save_path = os.path.join('/home/pasa/deeplearning/tf_models/snake/', args.gpu)
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
+
     logger.configure()
-    set_global_seeds(args.seed)
-    env = gym_snake.envs.SnakeEnv(grid_size=[13, 13], unit_size=1, snake_size=4, unit_gap=0, action_transformer=LocalAction())
+    set_global_seeds(0)
+    env = gym_snake.envs.SnakeEnv(grid_size=[13, 13], unit_size=1, snake_size=5, unit_gap=0, n_foods=3, action_transformer=LocalAction())
     env = bench.Monitor(env, logger.get_dir())
     model = deepq.models.cnn_to_mlp(
         convs=[(32, 8, 1), (64, 4, 1), (64, 3, 1)],
@@ -40,19 +40,18 @@ def main():
         env,
         q_func=model,
         lr=1e-4,
-        max_timesteps=args.num_timesteps,
+        max_timesteps=400000,
         buffer_size=10000,
-        exploration_fraction=0.1,
-        exploration_final_eps=0.001,
-        #train_freq=4,
-        #learning_starts=5,
+        exploration_fraction=0.3,
+        exploration_final_eps=0.01,
         target_network_update_freq=1000,
         gamma=1.0,
-        prioritized_replay=bool(args.prioritized),
-        prioritized_replay_alpha=args.prioritized_replay_alpha,
-        checkpoint_freq=args.checkpoint_freq,
-        checkpoint_path=args.checkpoint_path,
+        prioritized_replay=False,
+        prioritized_replay_alpha=0.6,
+        checkpoint_freq=100,
+        checkpoint_path=save_path,
         print_freq=10,
+        batch_size=64,
         #callback=callback
     )
 
