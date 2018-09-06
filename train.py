7b991ff77db5477d02b25357a72b42550c20d0ea
Andreas Pasternak <andreas.pasternak@gmx.ch>
2018-09-07 00:19:43 +0200

    2018-09-06_22-53-56/tb

--- train.py
+++ train.py
@@ -80,7 +80,7 @@ def main():
     num_timesteps = 4e8
 
     policy =  CnnPolicy
-    model = ppo2.learn(policy=policy, env=env, nsteps=2048, nminibatches=128, gamma=0.9,
+    model = ppo2.learn(policy=policy, env=env, nsteps=2048, nminibatches=128, gamma=0.8,
         noptepochs=10, log_interval=10,
         ent_coef=.005,
         lr=lambda f : f * 5e-5,
